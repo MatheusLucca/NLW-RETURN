98cd1651eeb361548e8f55f29164d75a9276669c
Matheus Lucca <mat.lualves@gmail.com>
2022-05-04 15:29:25 -0300

    create feature sendEmail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
-const app = express()
+const app = express();
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f7502a99458eba",
+      pass: "f523b6c1977b68"
+    }
+  });
 
 app.post('/feedbacks', async (req, res) =>{
     const {type, comment, screenshot} = req.body
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) =>{
             screenshot,
         }
     })
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Matheus Lucca <mat.lualves@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo do feedback: ${type} </p>`,
+            `<p> Comentário: ${comment} </p>`,
+            '</div>'
+        ].join('\n')
+    });
     return res.status(201).json({data : feedback})
 })
 
